e2ca1c434183e8d7289c82e92bb7043d8d5dda61
roger paalberg <roger.paalberg@techno.ee>
2026-03-26 22:49:49 +0200

    valmis

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const button = document.querySelector('button');
-
+const keha = document.querySelector('div');
 button.addEventListener('click', async function() {
  const response = await fetch('https://api.imgflip.com/get_memes');
   const data = await response.json();
-  console.log(data.temperature);
+
+ let pilt = (data.data.memes[Math.floor(Math.random() * 99)].url);
+ let meme = document.createElement("img");
+ meme.src = pilt;
+ 
+ keha.replaceChildren();
+ keha.appendChild(meme);
+ 
 });
 
 
