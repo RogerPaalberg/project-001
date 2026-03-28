19a4df00c8077b68ced4348a1feac0c4a49a4a5e
roger paalberg <roger.paalberg@techno.ee>
2026-03-28 12:20:32 +0200

    upgrade

--- script.js
+++ script.js
@@ -14,6 +14,27 @@ button.addEventListener('click', async function() {
 });
 
 
+const am = document.querySelector(".am");
+am.addEventListener('click', async function() {
+ const response = await fetch('https://api.imgflip.com/get_memes');
+  const data = await response.json();
+
+ let pilt = (data.data.memes[Math.floor(Math.random() * 99)].url);
+  let piltt = (data.data.memes[Math.floor(Math.random() * 99)].url);
+   let pilttt = (data.data.memes[Math.floor(Math.random() * 99)].url);
+ let memem = document.createElement("img");
+ memem.src = pilt;
+ let mememe = document.createElement("img");
+ mememe.src = piltt;
+ let mememm = document.createElement("img");
+ mememm.src = pilttt;
+ 
+ keha.replaceChildren();
+ keha.appendChild(memem);
+  keha.appendChild(mememe);
+  keha.appendChild(mememm);
+});
+
 
 
 
